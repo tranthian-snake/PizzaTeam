267656262bbbddd754b5754934e4eec7fb077539
an <54616933+tranthian-snake@users.noreply.github.com>
2020-11-28 14:52:31 +0700

    EAP

---
+++ Pizza_TeamVit/Pizza_TeamVit/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Pizza_TeamVit.Models;
+
+namespace Pizza_TeamVit.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index() => View();
+
+
+    }
+}
